2d773ca9d0e279b60b1c2a22ec482ea82d93bd89
shaolin-peanut <shaolinpeanut@gmail.com>
2024-12-11 16:42:50 +0100

    print string reverse (decrease iterator)

---
+++ module1/ex05/ft_print_rev.ml
@@ -0,0 +1,16 @@
+let ft_print_rev str =
+        let rec print_next (n : int) =
+                if n < 0 then print_char '\n'
+                else (
+                        print_char (String.get str n);
+                        print_next (n - 1)
+                )
+        in
+        print_next (String.length str - 1)
+
+
+let () =
+        ft_print_rev "Hello world!";
+        ft_print_rev "wouah";
+        ft_print_rev "";
+        ft_print_rev "1";
