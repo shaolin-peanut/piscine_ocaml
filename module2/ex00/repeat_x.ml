77e7631545360b628f38a7da5fe7323b6c1689c7
shaolin-peanut <shaolinpeanut@gmail.com>
2024-12-13 12:26:05 +0100

    exo 0 and 1 removed useless cond branch

--- module2/ex00/repeat_x.ml
+++ module2/ex00/repeat_x.ml
@@ -1,15 +1,13 @@
 let rec repeat_x n =
     if n < 0 then
         "Error"
-    else if n < 2 then
-        "x"
+    else if n = 0 then
+        ""
     else
         "x" ^ repeat_x (n - 1)
 
 let () = (
-    print_endline (repeat_x (-1));
-    print_endline (repeat_x 0);
-    print_endline (repeat_x 1);
-    print_endline (repeat_x 2);
-    print_endline (repeat_x 5);
+    List.iter (
+        fun n -> print_endline (repeat_x n)
+    ) [-1; 0; 1; 2; 5; 50]
 )
--- module2/ex01/repeat_string.ml
+++ module2/ex01/repeat_string.ml
@@ -3,50 +3,20 @@ let rec repeat_string ?str:(str1="x") n =
         "Error"
     else if n = 0 then
         ""
-    else if n = 1 then
-        str1
     else
         str1 ^ repeat_string ~str:str1 (n - 1)
 
 let () = (
-    print_endline (repeat_string (-1));
-    print_endline (repeat_string 0);
-    print_endline (repeat_string 2);
-    print_endline (repeat_string ~str:"yeah " 5);
-    print_endline (repeat_string ~str:"segfault " 10);
-    print_endline (repeat_string ~str:"segfault " (-1));
-    print_endline (repeat_string ~str:"a" 100);
-    print_endline (repeat_string ~str:"a" 0);
-    print_endline (repeat_string ~str:"what \n" 15);
+  (* without the string argument *)
+  List.iter (
+      fun n ->print_endline (repeat_string n)
+  ) [-1; 0; 2; 50];
+  (* to replace the ones with two arguments I need some pattern matching and passing tuples to the function, doing later *)
+  print_endline (repeat_string ~str:"yeah " (-1));
+  print_endline (repeat_string ~str:"yeah " 5);
+  print_endline (repeat_string ~str:"segfault " 10);
+  print_endline (repeat_string ~str:"segfault " (-1));
+  print_endline (repeat_string ~str:"a" 100);
+  print_endline (repeat_string ~str:"a" 0);
+  print_endline (repeat_string ~str:"what \n" 15);
 )
-
-
-(* let rec repeat_x n str =
-  if n < 0 then
-    "Error"
-  else if n < 2 then
-    str
-  else
-    str ^ repeat_x (n - 1) str
-  if String.length str = 0 then
-    let str = "x" in
-    repeat_x n str
-  else
-    let str = "x" ^ str in
-    repeat_x n str
-
-let rec repeat_x n str =
-  if n < 0 then
-      "Error"
-  else if n < 2 then
-      str
-  else
-      str ^ repeat_x (n - 1) str
-
-let () = (
-  print_endline (repeat_x (-1));
-  print_endline (repeat_x 0);
-  print_endline (repeat_x 1);
-  print_endline (repeat_x 2);
-  print_endline (repeat_x 5);
-) *)
