bfac04c48e46b1e54da44fccc6f51a087432e8bd
shaolin-peanut <shaolinpeanut@gmail.com>
2024-12-14 15:36:30 +0100

    final module2

--- module2/ex04/fibonacci.ml
+++ module2/ex04/fibonacci.ml
@@ -10,6 +10,7 @@
     -1
   else
     fibonacci_acc n 0 *)
+
 let fibonacci n =
   let rec fibonacci_acc n current next =
     if n = 0 then current
@@ -19,11 +20,13 @@ let fibonacci n =
     -1
   else
     fibonacci_acc n 0 1
-    
+
+
 let () = (
   List.iter ( fun n ->
+    print_string @@ "[" ^ (string_of_int n) ^ "]: ";
     fibonacci n |> string_of_int |> print_endline)
-    [(-42); 0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10];
+    [(-42); 0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 50];
   
   assert (fibonacci 0 = 0);
   assert (fibonacci 1 = 1);
@@ -44,5 +47,6 @@ let () = (
   assert (fibonacci 16 = 987);
   assert (fibonacci 17 = 1597);
   assert (fibonacci 18 = 2584);
-  assert (fibonacci 19 = 4181)
+  assert (fibonacci 19 = 4181);
+  assert (fibonacci 50 = 12586269025)
 )
--- module2/ex05/hofstader_mf.ml
+++ module2/ex05/hofstader_mf.ml
@@ -15,6 +15,13 @@ let () =
     [0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10];
 
   assert (hfs_m 0 = 0);
+  assert (hfs_m 1 = 0);
+  assert (hfs_m 2 = 1);
+  assert (hfs_m 3 = 2);
+  assert (hfs_m 4 = 2);
+  assert (hfs_m 5 = 3);
+  assert (hfs_m 6 = 4);
+  assert (hfs_m 7 = 4);
   assert (hfs_f 0 = 1);
   assert (hfs_m 4 = 2);
   assert (hfs_f 4 = 3);
