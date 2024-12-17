ea7487408ed17225968931e46f7a1584adddfc06
shaolin-peanut <shaolinpeanut@gmail.com>
2024-12-17 15:09:58 +0100

    oups version 5.3.0 doesn't exist

--- install_ocaml.sh
+++ install_ocaml.sh
@@ -9,5 +9,5 @@ echo "Yes I need to update everything to not break dependencies, arch policy..."
 pacman -Syu --noconfirm opam rsync git gcc make patch
 opam init --bare -a -y
 source ~/.bash_profile
-opam switch create piscine ocaml-base-compiler.5.3.0
+opam switch create piscine ocaml-base-compiler.5.2.0
 echo "should be all good, try compiling some *.ml files"
