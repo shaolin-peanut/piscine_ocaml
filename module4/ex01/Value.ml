94d03b3c48d833952ddfde6e284f40e4861e462e
shaolin-peanut <shaolinpeanut@gmail.com>
2024-12-29 23:25:25 +0100

    ex01 good, makefile update too

--- module4/ex01/Value.ml
+++ module4/ex01/Value.ml
@@ -12,7 +12,7 @@ let toInt (card : t) =
 let toString (card : t) : string = 
         match card with
         | T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9 | T10 ->
-                        string_of_int @@ toInt card
+                card |> toInt |> ( + ) 1 |> string_of_int
         | Jack -> "J"
         | Queen -> "Q"
         | King -> "K"
@@ -20,21 +20,20 @@ let toString (card : t) : string =
 
 let toStringVerbose (card : t) : string =
        match card with
-        | T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9 | T10 ->
-                        string_of_int @@ toInt card
+       | T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9 | T10 ->
+                card |> toInt |> ( + ) 1 |>
+                string_of_int |> ( ^ ) "T" 
        | Jack -> "Jack"
        | Queen -> "Queen"
        | King -> "King"
        | As -> "As"
 
 let next (card : t) : t=
-        if card = As then invalid_arg "Give a lower value card"
-        else
-                let rec find_next (prev : t) = function
-                        | [] -> invalid_arg
-                        | h :: t ->
-                                        if card = prev then h
-                                        else find_next h t
+        let rec find_next (prev : t) = function
+                | [] -> invalid_arg "Give a lower value card"
+                | h :: t ->
+                        if card = prev && card < h then h 
+                        else find_next h t
         in find_next T2 all
 
 let previous (card : t) : t =
@@ -43,7 +42,7 @@ let previous (card : t) : t =
                 let rec find_previous prev = function
                         | [] -> prev
                         | h :: t ->
-                                        if card = h then prev
-                                        else find_previous h t
-                in find_previous card all
+                                if card = h then prev
+                                else find_previous h t
+                in find_previous T2 all
 
--- module4/main.ml
+++
@@ -1,2 +0,0 @@
-(* write some blabla to
- show the types exist and work as intended *)
