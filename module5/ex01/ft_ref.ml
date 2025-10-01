225b911c45877c7a3db5d6244b14f9e80fde5e61
shaolin-peanut <shaolinpeanut@gmail.com>
2025-10-01 23:47:23 +0200

    ft_ref fixes

--- module5/ex01/ft_ref.ml
+++ module5/ex01/ft_ref.ml
@@ -4,8 +4,19 @@ let return x = { contents = x }
 
 let get r = r.contents
  
-let set r x = r.contents = x
+let set r x = r.contents <- x
 
-(* let bind (r : 'a ft_ref) f : 'a ft_ref = f r.contents *)
+let bind r f : 'b ft_ref = 
+  set r @@ f r.contents;
+  r
 
-let bind r f : 'b ft_ref = f r.contents
+let () =
+  let test_ref = return 5 in
+  Printf.printf "Printing the content of test_ref, an int: %d\n" @@ get test_ref;
+  let initial_ref = return 1 in
+  Printf.printf "Inital ref2 value: %d\n" @@ get initial_ref;
+  let () = set initial_ref 25 in
+  Printf.printf "Changed ref2 value: %d\n" @@ get initial_ref;
+  let timestwo x = x * 2 in
+  let bind_result = bind initial_ref timestwo in
+  Printf.printf "initial_ref with timestwo function applied to its content: %d\n" @@ get bind_result
